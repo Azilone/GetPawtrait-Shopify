2b0fd72b26f8fe4206c7be31e55b74e8a808f61f
Kevin <kevin.dejouet@gmail.com>
2025-06-05 07:43:42 +0200

    Initial commit: reset main

--- app/routes/hello.tsx
+++
@@ -1,8 +0,0 @@
-export default function HelloRoute() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Hello World!</h1>
-      <p>Welcome to your first Remix route.</p>
-    </div>
-  );
-}
